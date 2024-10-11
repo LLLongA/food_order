e21bf720d4da41c662d5f557bb190cebf62ae2ae
Long Chen <61738642+LLLongA@users.noreply.github.com>
2024-10-11 15:58:26 +1100

    write the basic framework of offer part

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div className="bg-black h-screen  flex flex-col md:flex-row">
+      <div className="flex-1">
+        <h1 className=""></h1>
+        <p className=""></p>
+        <button className=""></button>
+      </div>
+      <div className="flex-1 w-full relative">
+        <Image src="/offerProduct.png" alt="" fill className="object-contain" />
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
