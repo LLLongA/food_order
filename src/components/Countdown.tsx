933103837f0f06c2ee3b87b7f79d462d3882aa1c
Long Chen <61738642+LLLongA@users.noreply.github.com>
2024-10-12 17:22:48 +1100

    fix the error by  rendering the countdown only after the component has been mounted on the client

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,27 +2,32 @@
 import React, { useState, useEffect } from "react";
 
 const CountDown = () => {
-  let difference = +new Date(`1/1/2025`) - +new Date();
-  const [delay, setDelay] = useState(difference);
-
-  const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-  const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
-  const m = Math.floor((difference / 1000 / 60) % 60);
-  const s = Math.floor((difference / 1000) % 60);
+  const [isMounted, setIsMounted] = useState(false);
+  const [delay, setDelay] = useState(0);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setDelay(delay - 1);
-    }, 1000);
+    setIsMounted(true);
+    const difference = +new Date(`1/1/2025`) - +new Date();
+    setDelay(difference);
 
-    if (delay === 0) {
-      clearInterval(timer);
-    }
+    const timer = setInterval(() => {
+      setDelay((prevDelay) => prevDelay - 1000);
+    }, 1000);
 
     return () => {
       clearInterval(timer);
     };
-  });
+  }, []);
+
+  if (!isMounted) {
+    return null; // Don't render anything on the server.
+  }
+
+  const d = Math.floor(delay / (1000 * 60 * 60 * 24));
+  const h = Math.floor((delay / (1000 * 60 * 60)) % 24);
+  const m = Math.floor((delay / 1000 / 60) % 60);
+  const s = Math.floor((delay / 1000) % 60);
+
   return (
     <span className="font-bold text-5xl text-yellow-300">
       {d}:{h}:{m}:{s}
