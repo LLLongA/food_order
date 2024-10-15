695493255eebb1ae212739b5e4694f2fcd1ae593
Long Chen <61738642+LLLongA@users.noreply.github.com>
2024-10-15 11:13:15 +1100

    complete the design of OrderPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Featured from "@/components/Featured";
 import Offer from "@/components/Offer";
 import Slider from "@/components/Slider";
-import { div } from "framer-motion/client";
 
 export default function Home() {
   return (
